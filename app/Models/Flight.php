5ac1a8e25255978ac9cc98068ca4719ea454cf61
emincanozcan <emincanozcann@gmail.com>
2020-11-02 01:13:26 +0300

    Flight belongs to Airport relations defined

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -9,4 +9,13 @@ class Flight extends Model
 {
     use HasFactory;
     public $timestamps = false;
+
+    public function departureAirport()
+    {
+        return $this->belongsTo('App\Models\Airport', 'departure_airport', 'id');
+    }
+    public function arrivalAirport()
+    {
+        return $this->belongsTo('App\Models\Airport', 'arrival_airport', 'id');
+    }
 }

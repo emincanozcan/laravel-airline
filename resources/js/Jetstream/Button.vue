1e9b95ad92002205a7897eccd0a424b2af58b78e
emincanozcan <emincanozcann@gmail.com>
2020-11-02 03:07:35 +0300

    Emitting onClick event feature added to Button component

--- resources/js/Jetstream/Button.vue
+++ resources/js/Jetstream/Button.vue
@@ -1,16 +1,25 @@
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
-        <slot></slot>
-    </button>
+  <button
+    :type="type"
+    @click="click"
+    class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150 disabled:opacity-75 disabled:cursor-auto"
+  >
+    <slot></slot>
+  </button>
 </template>
 
 <script>
-    export default {
-        props: {
-            type: {
-                type: String,
-                default: 'submit',
-            },
-        }
-    }
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "submit",
+    },
+  },
+  methods: {
+    click(event) {
+      this.$emit("click", event);
+    },
+  },
+};
 </script>

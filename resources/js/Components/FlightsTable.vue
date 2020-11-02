ccd884bbeeb89a1444d2fff8c09ca9666bf1d068
emincanozcan <emincanozcann@gmail.com>
2020-11-02 03:09:32 +0300

    Flights table bug fixed & flight time added

--- resources/js/Components/FlightsTable.vue
+++ resources/js/Components/FlightsTable.vue
@@ -4,15 +4,16 @@
       <tr>
         <th class="px-4 py-2">From</th>
         <th class="px-4 py-2">To</th>
+        <th class="px-4 py-2">Flight Time</th>
         <th class="px-4 py-2">Price</th>
         <th class="px-4 py-2">Capacity</th>
       </tr>
     </thead>
     <tbody>
       <tr
-        v-for="flight in flights.data"
+        v-for="(flight, key) in flights.data"
         :key="flight.id"
-        :class="[flight.id % 2 == 0 ? 'bg-gray-50' : '']"
+        :class="[key % 2 == 0 ? 'bg-gray-50' : '']"
       >
         <td class="border px-4 py-2 text-center">
           {{ flight.departure_airport.full_name }}
@@ -33,6 +34,24 @@
           <span>
             {{ flight.arrival_time }}
           </span>
+        </td>
+        <td class="border px-4 py-2 text-center">
+          {{
+            Math.floor(
+              (new Date(flight.arrival_time) -
+                new Date(flight.departure_time)) /
+                3.6e6
+            )
+          }}
+          hours
+          {{
+            Math.floor(
+              (new Date(flight.arrival_time) -
+                new Date(flight.departure_time)) %
+                36e5
+            ) / 6e4
+          }}
+          minutes
         </td>
         <td class="border px-4 py-2 text-center">
           {{ flight.price }}

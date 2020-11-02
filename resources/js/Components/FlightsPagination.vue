8f8bd5a5054351305843868394fb4ee1e277ac1f
emincanozcan <emincanozcann@gmail.com>
2020-11-02 03:09:00 +0300

    Pagination improvements & refactor

--- resources/js/Components/FlightsPagination.vue
+++ resources/js/Components/FlightsPagination.vue
@@ -23,48 +23,37 @@
         </p>
       </div>
       <div>
-        <div class="relative">
-          <select
-            @change="(event) => changePage(event.target.value)"
-            class="block appearance-none w-full cursor-pointer bg-gray-100 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        <Select @change="(event) => changePage(event.target.value)">
+          <option
+            v-for="page in lastPage"
+            :key="page"
+            :selected="page === currentPage"
           >
-            <option
-              v-for="page in total"
-              :key="page"
-              :selected="page === currentPage"
-            >
-              Page {{ page }}
-            </option>
-          </select>
-          <div
-            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-          >
-            <svg
-              class="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path
-                d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-              />
-            </svg>
-          </div>
-        </div>
+            Page {{ page }}
+          </option>
+        </Select>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Select from "@/Components/Select";
 export default {
   props: ["total", "currentPage", "lastPage", "perPage"],
+  components: {
+    Select,
+  },
   methods: {
     changePage(page) {
       const trimmedPage = page.replace("Page ", "");
-
-      this.$inertia.visit(`/dashboard/flights?page=${trimmedPage}`, {
-        method: "get",
-      });
+      const params = new URLSearchParams(window.location.search);
+      let paramObj = {};
+      for (var value of params.keys()) {
+        paramObj[value] = params.get(value);
+      }
+      paramObj["page"] = page;
+      this.$inertia.get(`/dashboard/flights`, paramObj);
     },
   },
 };

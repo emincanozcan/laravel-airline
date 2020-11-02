3981526cb9ca32a493b665b0f57cfd098555f00d
emincanozcan <emincanozcann@gmail.com>
2020-11-02 03:10:00 +0300

    UI improvements

--- resources/js/Pages/Flight/Index.vue
+++ resources/js/Pages/Flight/Index.vue
@@ -8,17 +8,21 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
           <div
-            class="text-3xl my-4 pb-4 font-semibold text-center border-b border-gray-200"
+            class="my-4 pb-4 px-8 font-semibold text-center border-b border-gray-200 text-center"
           >
-            Flights
+            <h1 class="text-3xl">Flight List</h1>
           </div>
-          <div class="mx-6 mb-4">
+          <div>
+            <flights-filters class="px-8 mb-4" :airports="airports"/>
+          </div>
+
+          <div class="mx-8 mb-6">
             <flights-table :flights="flights" />
             <div>
               <flights-pagination
                 :total="flights.total"
                 :currentPage="flights.current_page"
-                :lastPage="flights.lastPage"
+                :lastPage="flights.last_page"
                 :perPage="flights.per_page"
               />
             </div>
@@ -32,13 +36,15 @@
 <script>
 import AppLayout from "@/Layouts/AppLayout";
 import FlightsTable from "@/Components/FlightsTable";
+import FlightsFilters from "@/Components/FlightsFilters";
 import FlightsPagination from "@/Components/FlightsPagination";
 export default {
-  props: ["flights"],
+  props: ["flights","airports"],
   components: {
     FlightsTable,
     AppLayout,
     FlightsPagination,
+    FlightsFilters,
   },
 };
 </script>
